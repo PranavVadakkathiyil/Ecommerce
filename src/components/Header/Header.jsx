acbcd39b6b4709580180023d145a518b8a925f60
Pranav Vadakkathiyil <vpranav0801@gmail.com>
2023-09-01 21:08:20 +0530

    Hader created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,52 @@
+import "./Header.scss";
+
+import { useEffect,useState } from 'react'; //useContext
+import {TbSearch} from "react-icons/tb"
+import {CgShoppingCart} from "react-icons/cg"
+import {AiOutlineHeart} from "react-icons/ai"
+
+
+function Header() {
+    const [Scrolled,setScrolled] = useState(false);
+    const handleScroll = () => {
+        const offset = window.scrollY;
+        if(offset > 200)
+        {
+            setScrolled(true);
+            
+        }
+        else {
+            setScrolled(false);
+        }
+    };
+
+    useEffect ( ()=>
+    {
+        window.addEventListener("scroll",handleScroll);
+    }, []);
+  return (
+    <header className={`main-header ${Scrolled ? 'sticky-header' : ''}`}>
+        <div className="header-content">
+            <ul className="left">
+                <li>Home</li>
+                <li>About</li>
+                <li>Categories</li>
+            </ul>
+            <div className="center">SmartShop</div>
+            <div className="right">
+                <TbSearch/>
+                <AiOutlineHeart/>
+                <span className="cart-icon">
+                    <CgShoppingCart/>
+                    <span>5</span>
+                </span>
+                
+                
+            </div>
+        </div>
+    </header>
+
+    )
+}
+
+export default Header
